a6f9943e62425d81fbae30fde75be0aa41762af8
liuhao521 <m18683346721@163.com>
2019-03-19 09:13:31 +0800

    添加了MessageVo

---
+++ src/main/java/com/company/project/vo/MessageVo.java
@@ -0,0 +1,22 @@
+package com.company.project.vo;
+
+import com.company.project.model.Message;
+
+public class MessageVo extends Message{
+	
+	private String realname;
+
+	public String getRealname() {
+		return realname;
+	}
+
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	
+	
+	
+	
+	
+
+}
